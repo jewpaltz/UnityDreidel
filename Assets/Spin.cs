a70f45468b2f70d60642c415496bc22b9ef5c830
Moshe Plotkin <plotkinm@newpaltz.edu>
2015-12-08 08:39:42 -0500

    first commit

---
+++ Assets/Spin.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+    private Rigidbody rigidBody;
+    public int minSpinSpeed = 500;
+    public int maxSpinSpeed = 1000;
+
+    // Use this for initialization
+    void Start () {
+        this.rigidBody = this.GetComponent<Rigidbody>();
+        rigidBody.centerOfMass = new Vector3(0, Random.Range(-.1f,.1f), .7f);
+        rigidBody.maxAngularVelocity = maxSpinSpeed;
+        rigidBody.AddTorque(0, Random.Range(minSpinSpeed, maxSpinSpeed), 0);
+        //rigidBody.angularVelocity = new Vector3(0, spinSpeed, 0);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Assets/click.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class click : MonoBehaviour {
+
+    public GameObject DriedelPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public Vector3 point;
+    void OnMouseDown()
+    {
+
+        //Vector3 mousePosition = Input.mousePosition;
+        //Debug.Log(mousePosition);
+        //mousePosition.z += 10;
+        //point = Camera.main.ScreenToWorldPoint(mousePosition);
+        //Debug.Log(point);
+
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 100.0f)) {
+            point = hit.point;
+            point.z -= 2;
+        }
+
+
+        Instantiate(DriedelPrefab, point, Quaternion.Euler(270,0,0));
+
+    }
+
+
+    void OnMouseDrag()
+    {
+    }
+}

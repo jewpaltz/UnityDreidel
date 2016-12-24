90b598bb1a9dea0609925ea119923f9c895e4e19
Moshe Plotkin <mplotkin@jewpaltz.com>
2016-12-24 18:57:50 -0500

    New Year

---
+++ Assets/GM.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GM : MonoBehaviour
+{
+    public List<Player> players = new List<Player>();
+    public int iCurrentPlayer = 0;
+    public int pot = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+        players.Add( new Player { name = "Moshe", points = 100 } );
+        players.Add( new Player { name = "Raphi", points = 100 } );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void EndOfTurn()
+    {
+        iCurrentPlayer = iCurrentPlayer++ % players.Count;
+
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+    public int points = 0;
+    public string name = "";
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
